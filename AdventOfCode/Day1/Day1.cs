700dd92767329427265245851df665386fd075ca
Darrexx <timo.walter@udo.edu>
2016-12-07 13:30:56 +0100

    Initial Commit

---
+++ AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
