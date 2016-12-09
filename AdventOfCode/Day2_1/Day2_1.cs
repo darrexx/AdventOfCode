33225a405bf3b796c1b3d867619b15a655b0e450
Darrexx <timo.walter@udo.edu>
2016-12-09 12:56:07 +0100

    Added Soluton for Day 2 Part 1

---
+++ AdventOfCode/Day2_1/Day2_1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_1
+{
+    class Day2_1
+    {
+        static void Main(string[] args)
+        {
+            var keyPad = new KeyPad();
+            var reader = new StreamReader("../../input.txt");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line != null)
+                {
+                    var moves = line.ToCharArray();
+                    foreach (var move in moves)
+                    {
+                        switch (move)
+                        {
+                            case 'U':
+                                keyPad.U();
+                                break;
+                            case 'R':
+                                keyPad.R();
+                                break;
+                            case 'D':
+                                keyPad.D();
+                                break;
+                            case 'L':
+                                keyPad.L();
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(nameof(moves), moves, null);
+                        }
+                    }
+                    Console.Write(keyPad.GetPosition());
+                }
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day2_1/KeyPad.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Day2_1
+{
+    public class KeyPad
+    {
+        private int position;
+
+        public KeyPad()
+        {
+            position = 5;
+        }
+
+        public int GetPosition()
+        {
+            return position;
+        }
+
+        public void ResetPosition()
+        {
+            position = 5;
+        }
+
+        public KeyPad U()
+        {
+            if (position == 1 || position == 2 || position == 3)
+            {
+                return this;
+            }
+            position -= 3;
+            return this;
+        }
+
+        public KeyPad R()
+        {
+            if (position == 3 || position == 6 || position == 9)
+            {
+                return this;
+            }
+            position += 1;
+            return this;
+        }
+
+        public KeyPad D()
+        {
+            if (position == 7 || position == 8 || position == 9)
+            {
+                return this;
+            }
+            position += 3;
+            return this;
+        }
+
+        public KeyPad L()
+        {
+            if (position == 1 || position == 4 || position == 7)
+            {
+                return this;
+            }
+            position -= 1;
+            return this;
+        }
+    }
+}
