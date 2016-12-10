2baf72a0de7bc1966d091835970ec7ee4bee8fc2
darrexx <timo.walter@udo.edu>
2016-12-10 13:23:44 +0100

    Added Solution for Day 6 Part 1

---
+++ AdventOfCode/Day6_1/Day6_1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_1
+{
+    class Day6_1
+    {
+        static void Main(string[] args)
+        {
+            var lines = new List<char[]>();
+            var reader = new StreamReader("../../input.txt");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line != null)
+                {
+                    lines.Add(line.ToCharArray());
+                }
+            }
+            for (int i = 0; i < lines[0].Length; i++)
+            {
+                var letters = new List<char>();
+                for (int j = 0; j < lines.Count; j++)
+                {
+                    letters.Add(lines[j][i]);
+                }
+                Console.Write(letters.GroupBy(x => x).OrderByDescending(x => x.Count()).Select(x => x.Key).ToArray()[0]);
+            }
+        }
+    }
+}
