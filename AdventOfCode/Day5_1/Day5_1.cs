7002ee574d1c1b154eac5fb1151832a455e25d41
Darrexx <timo.walter@udo.edu>
2016-12-10 01:42:12 +0100

    Added Solution for Day 5 Part 1.

---
+++ AdventOfCode/Day5_1/Day5_1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_1
+{
+    class Day5_1
+    {
+        static void Main(string[] args)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var reader = new StreamReader("../../input.txt");
+                var line = reader.ReadLine();
+                var i = 0;
+                var counter = 0;;
+                if (line != null)
+                {
+                    while(counter < 8)
+                    {
+                        var lineWithIndex = line + i;
+                        var byteArray = md5.ComputeHash(Encoding.UTF8.GetBytes(lineWithIndex));
+                        var hexCharArray = BitConverter.ToString(byteArray).Replace("-","").ToCharArray();
+                        if (hexCharArray[0] == '0' && hexCharArray[1] == '0' && hexCharArray[2] == '0' &&
+                            hexCharArray[3] == '0' && hexCharArray[4] == '0')
+                        {
+                            Console.Write(hexCharArray[5]);
+                            counter++;
+                        }
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
